d6013cbab56959763a28ed7068116fce5adee7a4
shanalikhan007 <shanalikhan@hotmail.com>
2014-11-05 16:15:46 +0500

    service created components connected - completed V1

---
+++ EmiratesRacing/EmiratesRacing.Web/Scripts/Horse/UpdateSelectedHorseService.js
@@ -0,0 +1,19 @@
+﻿app.factory('UpdateSelectedHorse', Service);
+function Service($rootscope) {
+ 
+    var sharedService = {};
+
+    sharedService.resultJSON = '';
+
+    sharedService.prepForPublish = function (msg) {
+        this.resultJSON = msg;
+        this.publishItem();
+    };
+
+    sharedService.publishItem = function () {
+        $rootScope.$broadcast('handlePublish');
+    };
+
+    return sharedService;
+
+};
---
+++ EmiratesRacing/EmiratesRacing.Web/Scripts/Horse/pedigreeTemplateNodeDirective.js
@@ -0,0 +1,35 @@
+﻿
+
+
+app.directive('pedigreeTemplateNode', Directive);
+
+function Directive() {
+
+    return {
+        scope: {
+            rowSpan :'=rowspan', 
+            colSpan :'=colspan', 
+            horse:'=horse'
+
+        },
+        restrict: 'A',
+        templateUrl: '/StaticViews/PeDigreeNode.html',
+        controllerAs: 'node',
+        controller: function ($scope) {
+            
+           // alert($scope.name);
+            this.rowSpan = $scope.rowSpan;
+            this.colSpan = $scope.colSpan;
+            this.horse = $scope.horse;
+            var date = new Date($scope.horse.DateOfBirth);
+            $scope.horse.DateOfBirth = date.getFullYear();
+                
+                
+            
+            
+
+        }
+
+    };
+
+};
